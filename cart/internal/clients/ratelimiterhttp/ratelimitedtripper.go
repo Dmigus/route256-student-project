ecee1d195d5abb4e910a453381e9144c107f9363
Dmigus <44640095+Dmigus@users.noreply.github.com>
2024-04-01 21:50:10 +0300

    [GOHW-5]: Добавлены комментарии к RateLimitedTripper. Добавлены тесты многопоточности к InMemoryCartRepository

--- cart/internal/clients/ratelimiterhttp/ratelimitedtripper.go
+++ cart/internal/clients/ratelimiterhttp/ratelimitedtripper.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+// RateLimitedTripper это структура, которая позволяет выполнять RoundTrip раунд трипера next нен чаще rps запросов в секунду
 type RateLimitedTripper struct {
 	rl   ratelimit.Limiter
 	next http.RoundTripper
 }
 
+// NewRateLimitedTripper создаёт новый экземпляр RateLimitedTripper
 func NewRateLimitedTripper(rps int, next http.RoundTripper) *RateLimitedTripper {
 	return &RateLimitedTripper{
 		rl:   ratelimit.New(rps),
@@ -17,6 +19,7 @@ func NewRateLimitedTripper(rps int, next http.RoundTripper) *RateLimitedTripper 
 	}
 }
 
+// RoundTrip обёртка над RoundTrip вложенного раунд трипера с ограничением по rps.
 func (rlc *RateLimitedTripper) RoundTrip(r *http.Request) (*http.Response, error) {
 	rlc.rl.Take()
 	return rlc.next.RoundTrip(r)
