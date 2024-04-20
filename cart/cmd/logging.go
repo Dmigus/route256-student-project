dbedad11947e800400b500895448a2f775b448fe
Dmigus <44640095+Dmigus@users.noreply.github.com>
2024-04-20 08:47:31 +0300

    [GOHW-7]: Добавлено указание уровня логирования

--- cart/cmd/logging.go
+++ cart/cmd/logging.go
@@ -3,12 +3,17 @@ package main
 import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"log"
 	"time"
 )
 
 func getLogger() *zap.Logger {
 	cfg := zap.NewProductionConfig()
-	cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	level, err := zap.ParseAtomicLevel(cliFlags.loggerLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg.Level = level
 	cfg.OutputPaths = []string{"stdout"}
 	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.RFC3339)
 	logger := zap.Must(cfg.Build(zap.WithCaller(false)))
