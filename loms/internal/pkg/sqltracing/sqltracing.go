921bd65cbe747103de7a44ba6c912f512dab4a27
Dmigus <44640095+Dmigus@users.noreply.github.com>
2024-05-11 18:30:15 +0300

    фикс бага в loms

--- loms/internal/pkg/sqltracing/sqltracing.go
+++ loms/internal/pkg/sqltracing/sqltracing.go
@@ -16,7 +16,6 @@ func CreateConnToPostgres(dsn string) (*pgxpool.Pool, error) {
 	}
 	cfg.ConnConfig.Tracer = otelpgx.NewTracer(otelpgx.WithTrimSQLInSpanName())
 	conn, err := pgxpool.NewWithConfig(context.Background(), cfg)
-	conn.Close()
 	if err != nil {
 		return nil, fmt.Errorf("connect to database: %w", err)
 	}
